1f345e5972723a828067f69c9cdbef5941aa05b6
“Haven-jiang” <Haven-just@qq.com>
2021-08-09 21:59:53 +0800

    Add files via upload

--- Algorithm/src/cn/itcast/algorithm/map/unMap.java
+++ Algorithm/src/cn/itcast/algorithm/map/unMap.java
@@ -84,5 +84,6 @@ public class unMap {
         unMap un = new unMap(new char[]{'a', 'b', 'c', 'd', 'v', 'o'});
         un.setSide(new char[]{'a', 'd', 'c', 'b', 'b', 'd', 'a', 'o'});
         System.out.println(un);
+        //test
     }
 }
